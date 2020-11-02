0b3c62db0ead93d37f6471ed1b575c6455328f50
Umberto Biccari <umberto.biccari@deusto.es>
2020-11-02 11:07:45 +0100

    memoria técnica

--- exampleJanabi/algebraicValidation.m
+++ exampleJanabi/algebraicValidation.m
@@ -2,8 +2,8 @@ function answ = algebraicValidation(p,g)
 
 algebraicEq = zeros(1,0.5*length(g));
 
-algebraicEq(1) = g(1)-2*p(1);
-algebraicEq(2) = g(2)-p(1)*g(1);
+algebraicEq(1) = g(1) - 2*p(1);
+algebraicEq(2) = g(2) - p(1)*g(1);
 algebraicEq(3) = g(3) - p(1)*g(2) + p(2)*g(1) - 2*p(3); 
 algebraicEq(4) = g(4) - p(1)*g(3) + p(2)*g(2) - p(3)*g(1);
 
